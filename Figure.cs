fc0c632a110e2bb49effe927358b3f1dfc9dff4a
2Maxxi <vorvulev4@fmail.com>
2023-02-18 21:44:35 +0500

    first commit

---
+++ Figure.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Практика_11_12_Зад2_В9
+{
+    public class Figure
+    {
+        public Double[,] arr = new double[3, 2];
+
+        int figur = 0;
+        public void rectangle(Double dlina, Double shirina)
+        {
+            try
+            {
+                Double ploshad = dlina * shirina;
+                Double pirimetr = 2 * (dlina + shirina);
+                arr[0, 0] = ploshad;
+                arr[0, 1] = pirimetr;
+            }
+            catch
+            {
+                arr[0, 0] = 0;
+                arr[0, 1] = 0;
+                MessageBox.Show("В данных прямоугольника введены неверные значения");
+            }
+        }
+
+        public void Circle(Double radius)
+        {
+            try
+            {
+                Double ploshad = 3.14 * Math.Pow(radius, 2);
+                Double pirimetr = 2 * 3.14 * radius;
+                arr[1, 0] = ploshad;
+                arr[1, 1] = pirimetr; 
+            }
+            catch
+            {
+                arr[1, 0] = 0;
+                arr[1, 1] = 0;
+                MessageBox.Show("В данных круга введены неверные значения");
+            }
+        }
+
+        public void Triangle(Double osnovanie, Double visota, Double a, Double b, Double c)
+        {
+            try
+            {
+                Double ploshad = 0.5 * osnovanie * visota;
+                Double pirimetr = a + b + c;
+                arr[2, 0] = ploshad;
+                arr[2, 1] = pirimetr;
+            }
+            catch
+            {
+                arr[2, 0] = 0;
+                arr[2, 1] = 0;
+                MessageBox.Show("В данных треугольника введены неверные значения");
+            } 
+        }
+        public string [,] Vizov()
+        {
+           string [,] a = Vivod(arr);
+            return a;
+        }
+        
+        public String[,] Vivod(double[,] arr)
+        {
+            string [,] a = new string[3, 2];
+            int i= 0;
+            while (i<3)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    a[i, j] = Convert.ToString(arr[i, j]);
+                }
+                i++;
+            }
+            return a;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_11_12_Зад2_В9
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        Figure a = new Figure();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int q) & int.TryParse(textBox2.Text, out int w))
+            {
+                a.rectangle(Convert.ToDouble(textBox1.Text), Convert.ToDouble(textBox2.Text));
+            }
+            else
+            {
+                MessageBox.Show("В данных прямоугольника введены неверные значения");
+            }
+
+            if (textBox3.Text != "")
+            {
+                a.Circle(Convert.ToDouble(textBox3.Text));
+            }
+            else
+            {
+                MessageBox.Show("В данных круга введены неверные значения");
+            }
+            
+            if (textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "" &&  textBox7.Text != "" && textBox8.Text != "")
+            {
+                a.Triangle(Convert.ToDouble(textBox4.Text), Convert.ToDouble(textBox5.Text), Convert.ToDouble(textBox6.Text),Convert.ToDouble(textBox7.Text), Convert.ToDouble(textBox8.Text));
+            }
+            else
+            {
+                MessageBox.Show("В данных треугольника введены неверные значения");
+            }
+
+            string[,] h = a.Vizov();
+            int i = 0;
+            while (i < 3)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    if (i == 0 & j == 1)
+                    {
+                        label12.Text = $"Прямоугольник:\nПлощадь-{h[i,0]} Периметр-{h[i, 1]}\n";
+                    }
+
+                    if (i == 1 & j == 1)
+                    {
+                        label12.Text = label12.Text+ $"Круг:\nПлощадь-{h[i, 0]} Периметр-{h[i, 1]}\n" ;
+                    }
+
+                    if (i == 2 & j == 1)
+                    {
+                        label12.Text = label12.Text +$"Треугольник:\nПлощадь-{h[i, 0]} Периметр-{h[i, 1]}\n";
+                    }  
+                }
+                i++;
+            }
+            label12.Visible = true;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
